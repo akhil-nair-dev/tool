b83504a73dd0fbc2e86e165e1e603873a92b0961
AkhilNair93 <akhil@makemine.co>
2020-05-06 11:08:12 +0530

    mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,4 +4,10 @@ class ApplicationMailer < ActionMailer::Base
 end
 
  class UserMailer < ApplicationMailer
+ 	default from: 'test@example.com'
+
+  def send_welcome_email(user)
+    @user = user
+    mail(:to => @user.email, :subject => "Welcome!")
+  end
  end

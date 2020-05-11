db96f6808a6f8abac8060e10bdd841fed071446e
AkhilNair93 <akhil@makemine.co>
2020-05-11 11:41:07 +0530

    deleted groupify

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,30 +1,29 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
-  # GET /projects
-  # GET /projects.json
-  def index
-    @projects = Project.all
+    def index
+    @projects = Project.all.order("created_at DESC")
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
+  
   def show
   end
 
-  # GET /projects/new
+  
   def new
-    @project = Project.new
+    @project = current_user.projects.build
+    @teams = Team.where('id = ?', current_user.team_id)
   end
 
-  # GET /projects/1/edit
+  
   def edit
+    @teams = current_user.teams
   end
 
-  # POST /projects
-  # POST /projects.json
+  
   def create
-    @project = Project.new(project_params)
+    @project = current_user.projects.build(project_params)
 
     respond_to do |format|
       if @project.save
@@ -37,8 +36,7 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
+  
   def update
     respond_to do |format|
       if @project.update(project_params)
@@ -51,24 +49,22 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
+  
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_project
       @project = Project.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:name, :description)
+      params.require(:project).permit(:name, :description, :team_id)
     end
 end
